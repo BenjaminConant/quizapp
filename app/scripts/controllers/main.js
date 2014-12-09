11eef104141b354e24b6e107c46b127bfc3f9cf4
Chuck Pierce <chuckmpierce@gmail.com>
2014-12-09 18:03:11 -0500

    made the question update automatic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,43 +20,53 @@ angular.module('quizApp')
       "q": "Who is the best ping pong player at FSA?",
       'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {'value' : "Nimit"} , { 'value': "Patrick"}],
       'answer': "Nimit",
-      'difficulty': 2
+      'difficulty': 2,
+      'answered': false
     },
     { "q": "Which robot name was chanted during Lego Mindstorms?",
-    'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {'value' : 'johnny5'} , { 'value': 'none of the above'}],
+    'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js' } , {'value' : 'johnny5'} , { 'value': 'none of the above'}],
     'answer':'noHope.js',
-    'difficulty': 9
+    'difficulty': 9,
+    'answered': false
   },
   {
     'q': "Out of the following frontend frameworks, which framework is most rails-like",
     'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {'value' : 'Backbone.js'} , { 'value': 'Meteor.js'}],
     'answer':'Ember.js',
-    'difficulty': 4
+    'difficulty': 4,
+    'answered': false
   },
   {
     'q': "Which project used a local data store?",
     'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {'value' : 'WikiWalker'} , { 'value': 'WikiStack'}],
     'answer':'Twitter.js',
-    'difficulty': 3
+    'difficulty': 3,
+    'answered': false
   }
   ];
 
   $scope.userPoints = 0;
-  $scope.message = "welcome to the game"
+  $scope.message = "welcome to the game";
 
-  $scope.checkAnswer = function(questionAnswer, userAnswer) {
+  $scope.random = function() {
+        return 0.5 - Math.random();
+  }
 
+  $scope.checkAnswer = function(question, userAnswer) {
 
-    if (questionAnswer === userAnswer) {
-      $scope.message = "your the best you ++ 7000pts";
-      $scope.userPoints += 7000;
-      $scope.messageColorGreen = true;
-      $scope.messageColorRed = false;
-    } else {
-      $scope.message =  "your wrong you fool!!! - 10,000";
-      $scope.userPoints -= 10000;
-      $scope.messageColorRed = true;
-      $scope.messageColorGreen = false;
+    if(!question.answered) {
+      if (question.answer === userAnswer) {
+        $scope.message = "your the best you ++ 7000pts";
+        $scope.userPoints += 7000;
+        $scope.messageColorGreen = true;
+        $scope.messageColorRed = false;
+        question.answered = true;
+      } else {
+        $scope.message =  "your wrong you fool!!! - 10,000";
+        $scope.userPoints -= 10000;
+        $scope.messageColorRed = true;
+        $scope.messageColorGreen = false;
+      }
     }
 
   };
@@ -76,14 +86,14 @@ angular.module('quizApp')
   $scope.newQuestion = {
     'q': "",
     'options': [],
-    'answer': "",
-    'difficulty': 0,
+    'answer': ""
   };
 
   $scope.addQuestion = function() {
     $scope.newQuestion.answer = $scope.newQuestion.options[0].value;
     $scope.quiz.push($scope.newQuestion);
     console.log($scope.quiz);
+    $scope.newQuestion = {'options': [], 'answered': false};
   }
 
   });
